78a4efb89672563457ff72e56fc41d7984afcc2b
Yasin Onur Gürbüz <33712084+yasinonurgurbuz@users.noreply.github.com>
2021-09-20 13:34:39 +0300

    Fixing the Pet Store Test Class
    Fixing the Pet Store Test Class

--- src/test/java/com/petsore/testcases/PetStoreTests.java
+++ src/test/java/com/petsore/testcases/PetStoreTests.java
@@ -10,45 +10,40 @@ import static org.hamcrest.Matchers.equalTo;
 public class PetStoreTests extends PetStoreBaseTests{
     @Test
     public void it_should_create_user_for_pet_store(){
-
-        //Given : Create a user for PetStore
-        var username = "mehmet";
-        var firstName = "Mehmet;";
+        //Given : Create an user for PetStore
+        var username = "userName";
+        var firstName = "User First Name;";
         var userCreateRequest = UserCreateRequest
                 .builder()
                 .id(111L)
                 .username(username)
                 .firstName(firstName)
-                .lastName("Gürbüz")
-                .email("deneme@gmail.com")
-                .password("1234asdf")
+                .lastName("User Last Name")
+                .email("useremail@gmail.com")
+                .password("1234user")
                 .phone("095555555555")
                 .userStatus(1L)
                 .build();
         setUserAccountForPetStore(userCreateRequest);
 
-        //When : Set package to the archive with migration
-        var updateFirstName = "Ahmet";
+        //When : Update the user information on Pet Store
+        var updateFirstName = "User Update First Name";
         var userUpdateRequest = UserUpdateRequest
                 .builder()
                 .id(111L)
                 .username(username)
                 .firstName(updateFirstName)
-                .lastName("Gürbüz")
-                .email("updatedemail@gmail.com")
-                .password("updatedPassword")
-                .phone("updatedPhoneNumber")
+                .lastName("User Update Last Name")
+                .email("updateduseremail@gmail.com")
+                .password("1234updateduser")
+                .phone("095555555555")
                 .userStatus(1L)
                 .build();
-
         setUserInformationUpdateRequest(username,userUpdateRequest);
 
         //Then : Check the values for updated user Information
         var petStoreUpdatedUserResponse = getUpdatedPetStoreUserInformationWithUserName(username);
         var updatedFirstName = petStoreUpdatedUserResponse.getFirstName();
-
-
-
-        assertThat("Check the orderNumber before the migrate", updateFirstName, equalTo(updatedFirstName));
+        assertThat("Check the updateFirstName is equal to updatedFirstName", updateFirstName, equalTo(updatedFirstName));
     }
 }
